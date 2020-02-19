50a76c181d9c893f99ca8ac7b245f2a41123242e
Lyrklif <kochurow121@gmail.com>
2020-02-19 18:26:58 +0400

    3rf170

--- src/redmine/getStatistics.js
+++ src/redmine/getStatistics.js
@@ -3,22 +3,35 @@ import {setStoreTasks} from '../actions/actionCreators';
 
 
 // получить
+//TODO нужно выводить статистику за: день, неделю, месяц
 const getStatistics = (url, api) => {
-  const globalTimer = new Date();
-  // stringToSplit.split(separator)
-  console.log('globalTimer', globalTimer);
-  console.log('globalTimer', globalTimer.toISOString());
-  let today = 'from=2020-02-19&to=2020-02-19';
-  return axios.get(`${url}/time_entries.json?key=${api}&${today}&user_id=me&=hours&limit=100`)
-    .then(response => {
-      // let tasks = JSON.parse(response.request.response);
+  const date = new Date();
 
-      // setStoreTasks(tasks.issues); // записать задачи
+  const _year = date.getFullYear();
+  const _month = (date.getMonth() + 1) >= 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`; // чтобы отчёт начинался с 1, а не с 0
+  const _day = date.getDate() >= 0 ? date.getDate() : `0${date.getDate()}`;
+  const _weekday = date.getDay() === 0 ? 6 : date.getDay() - 1; // чтобы начинать отсчёт с Пн, а не в Вс
+  const _firstDayWeek = (+_day - +_weekday) >= 10 ? +_day - +_weekday : `0${+_day - +_weekday}`;
+
+  const today = `${_year}-${_month}-${_day}`;
+  const weekStarted = `${_year}-${_month}-${_firstDayWeek}`;
+  const monthStarted = `${_year}-${_month}-01`;
+
+  const commandToday = `from=${today}&to=${today}`;
+  const commandWeek = `from=${weekStarted}&to=${today}`;
+  const commandMonth = `from=${monthStarted}&to=${today}`;
+
+  console.log('commandToday', commandToday);
+  console.log('commandWeek', commandWeek);
+  console.log('commandMonth', commandMonth);
+
+  return axios.get(`${url}/time_entries.json?key=${api}&${commandToday}&user_id=me&=hours&limit=100`)
+    .then(response => {
 
       let data = JSON.parse(response.request.response);
-      let hours = data.time_entries;
+      let array = data.time_entries;
 
-      console.log("getStatistics => response \n", data);
+      console.log("getStatistics => response \n", array);
 
       return response;
     })
